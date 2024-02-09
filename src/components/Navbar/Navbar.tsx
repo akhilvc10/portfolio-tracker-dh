3aff91fb0401545951b4b4f9dbf594b299a33d82
Akhil VC <akhilvc10@gmail.com>
2024-02-09 16:58:18 +0400

    fix():attempt2 - fix ui issue nav icon

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -55,14 +55,23 @@ export default function Navbar({ links }: { links: Array<NavLinkTypes> }) {
 					<Button
 						size="icon"
 						onClick={() => setMobileMenuOpen(true)}
-						className="p-1 lg:hidden bg-bg-color text-typography-1"
-						variant="ghost"
+						className="p-1 lg:hidden bg-card-bg text-typography-1"
+						variant="outline"
 					>
-						<Bars3Icon
-							color="var(--color-typography-1)"
-							className="!text-typography-1"
-							aria-hidden="true"
-						/>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="var(--color-typography-1)"
+							viewBox="0 0 24 24"
+							strokeWidth={1.5}
+							stroke="currentColor"
+							className="w-6 h-6"
+						>
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+							/>
+						</svg>
 					</Button>
 				</div>
 
